f807880408a09d4cc40642bec3d7514df95888e4
omarshokry11 <omarshokry11>
2021-06-02 12:11:28 +0200

    Create Business From

--- src/Layout/pages/Home.js
+++ src/Layout/pages/Home.js
@@ -1,25 +1,50 @@
 import React from "react";
-import {Image} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import CoverHome from "../component/Block/CoverHome";
 
 import "./style/Home.scss";
 
 export default function Home(){
     return(
         <div className="main-home-content-container">
-            <div className="container-fluid">
+            <div className="cover-home">
+                <CoverHome />
+            </div>
+            <div className="container content-home">
                 <div className="row">
-                    <div className="col-lg-6 col-12 item-text">
-                        <div className="text-details">
-                            <h1>Managed Analytics and Data Analysis Platform</h1>
-                            <p>We have created a cloud based tool to do your all boring tasks related to data analysis and decesion making based on the nature of data!</p>
+                    <div className="col-12 text-center title-details">
+                        <h2>How We Take Your Business From Good To Great</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-12 item item-1">
+                        <div className="icon">
+                            <i className="fas fa-search"> </i>
                         </div>
-                        <div className="button">
-                            <button className="link-btn"><i className="fas fa-arrow-right"> </i>About US</button>
-                            <button className="link-btn link-btn-1"><i className="fas fa-check"> </i>Get Started</button>
+                        <div className="item-details">
+                            <h3>Analyze Your Data Needs</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                            <Link to="/"><i className="fas fa-arrow-right"> </i> Read More</Link>
                         </div>
                     </div>
-                    <div className="col-lg-6 col-12 img-cover">
-                        <Image src="/img/banner-four-main-img-6ade92c3d5c70b3c70c3d9908255842d.png" />
+                    <div className="col-lg-4 col-md-6 col-12 item item-2">
+                        <div className="icon">
+                            <i className="fas fa-chess-board"> </i>
+                        </div>
+                        <div className="item-details">
+                            <h3>Develop A Customized Plan</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                            <Link to="/"><i className="fas fa-arrow-right"> </i> Read More</Link>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-12 item item-3">
+                        <div className="icon">
+                            <i className="far fa-lightbulb"> </i>
+                        </div>
+                        <div className="item-details">
+                            <h3>Implement Your Solution</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                            <Link to="/"><i className="fas fa-arrow-right"> </i> Read More</Link>
+                        </div>
                     </div>
                 </div>
             </div>
