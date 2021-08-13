dab9b3e72ccfc9759de4f545c60f8787043ab6bc
omarshokry11 <omarshokry238@gmail.com>
2021-08-13 09:42:14 +0200

    Create Courses  Page

---
+++ src/Layout/pages/Courses.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CoverPages from "../component/Block/CoverPages";
+
+export default function Courses() {
+    return(
+        <div className="main-courses-container">
+            <div className="cover">
+                <CoverPages title="Course Grid" />
+            </div>
+        </div>
+    )
+}
